17fb4906307bc4431cd45a591c6b2663aecceac6
bdeeker <brian.deeker@gmail.com>
2026-06-11 15:50:41 +0000

    update backend IP to 98.92.22.137

--- medcred-web/next.config.ts
+++ medcred-web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://13.222.12.123:8080/api/:path*",
+        destination: "http://98.92.22.137:8080/api/:path*",
       },
     ];
   },
